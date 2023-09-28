6f4752f7da3b7a2152714dea065c339011879ca8
Varun Mayilavaganan <112771701+Varun-Mayilvaganan@users.noreply.github.com>
2023-09-28 13:00:02 +0530

    Create cardswipe.py

---
+++ cardswipe.py
@@ -0,0 +1,14 @@
+#Card Swipe
+
+for i in range(int(input())):
+    a = int(input())
+    lis = list(map(int,input().split()))
+    b = 0
+    newlist = set()
+    for j in range(a):
+        if lis[j] not in newlist:
+            newlist.add(lis[j])
+        else:
+            newlist.remove(lis[j])
+        b = max(b, len(newlist))
+    print(b)    
