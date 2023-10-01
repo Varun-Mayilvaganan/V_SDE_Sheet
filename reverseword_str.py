3f73a49b90a61254b5fa71ab5b499c19419c5427
Varun Mayilavaganan <112771701+Varun-Mayilvaganan@users.noreply.github.com>
2023-10-01 16:38:26 +0530

    Create reverseword_str.py

---
+++ reverseword_str.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        reversed_words=[]
+        for word in words:
+            reversed_word=word[::-1]
+            reversed_words.append(reversed_word)
+        return ' '.join(reversed_words)
