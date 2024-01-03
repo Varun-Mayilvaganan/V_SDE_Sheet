1c7a57f46d6afd1e0777fccaf96792e478058bbc
Varun <varunmayilvaganan11@gmail.com>
2024-01-03 19:56:06 +0530

    2d solved

--- 2d_array.py
+++ 2d_array.py
@@ -23,3 +23,16 @@
 # Output: [[4,3,2,1]]
 # Explanation: All elements of the array are distinct, so we can keep all of them in the first row of the 2D array.
 
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        count = defaultdict(int)
+        res = []
+
+        for n in nums:
+            row = count[n]
+            if len(res) == row:
+                res.append([])
+
+            res[row].append(n)
+            count[n] +=1
+        return res     
